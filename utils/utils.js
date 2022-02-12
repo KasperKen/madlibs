dc118004e81b2e6595ea95242bc3b93249e4054c
kasperken <kennethandrews@protonmail.com>
2022-02-12 07:38:31 -0500

    created a function to capitalize letters

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,10 @@
-const chooseRandom = (array) => {
-  const index = Math.floor(Math.random() * array.length);
-  return array[index];
+const chooseRandom = (object) => {
+  const index = Math.floor(Math.random() * object.length);
+  return object[index];
 };
 
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
 module.exports = {
-  chooseRandom,
+  chooseRandom, capitalizeFirstLetter,
 };
